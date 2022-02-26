037154cdb9ef4c698c6a4f25d5e6a6dd2d5532c5
cee breezy <djmisscbrown@hotmail.com>
2022-02-26 15:34:10 +0000

    Added new folder to develop

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,7 @@
 const fs = require('fs'); 
 const inquirer = require('inquirer'); 
 const generateMarkdown = require('./utils/generateMarkdown');
-const fileName = './Output/README.md';
+const fileName ='./Output/README.md';
 
 // TODO: Create an array of questions for user input
 const questions = [
